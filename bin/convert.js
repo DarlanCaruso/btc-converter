bd2c5e8084a495ea9336b50e93db08be540309f8
Darlan Caruso <devcaruso@gmail.com>
2018-01-21 17:16:23 -0200

    Add Spinner

--- bin/convert.js
+++ bin/convert.js
@@ -1,6 +1,12 @@
 'use strict';
 
 var chalk = require('chalk');
+var ora = require('ora');
+
+var spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow'
+});
 
 var request = require('request');
 //https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=USD&amount=2
@@ -9,9 +15,11 @@ function convert() {
   var amount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
 
   var url = 'https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=' + currency + '&amount=' + amount;
+  spinner.start();
 
   request(url, function (err, response, body) {
     var apiResponse = void 0;
+    spinner.stop();
     try {
       apiResponse = JSON.parse(body);
     } catch (parseError) {
--- src/convert.js
+++ src/convert.js
@@ -1,12 +1,20 @@
 const chalk = require('chalk');
-
+const ora = require('ora');
 const request = require('request');
+
+const spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow',
+});
+
 //https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=USD&amount=2
 function convert(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+  spinner.start();
 
   request(url, (err, response, body) => {
     let apiResponse;
+    spinner.stop();
     try {
       apiResponse = JSON.parse(body);
     } catch (parseError) {
